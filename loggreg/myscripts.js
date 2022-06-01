2dffbb35b8c71d4e9e873ec82f93b6d57eff0129
KnowlyBrown <85461535+KnowlyBrown@users.noreply.github.com>
2022-06-01 02:20:30 +0100

    Add files via upload

---
+++ loggreg/myscripts.js
@@ -0,0 +1,13 @@
+document.getElementById("btnn").onclick = showDetails;
+
+
+
+function showDetails() {
+
+  const x = document.getElementById("loginUser").value;
+  const y = document.getElementById("loginPassword").value;
+  
+  document.getElementById("userN").innerHTML = x;
+  document.getElementById("passs").innerHTML = y;
+}
+
